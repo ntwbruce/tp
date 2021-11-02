f33dc37f976db93cab4d826d2691d8b1fa1dc619
ntwbruce <e0544240@u.nus.edu>
2021-11-02 11:09:18 +0800

    Remove date changes
    This is the wrong PR for date changes.

--- src/test/java/seedu/address/testutil/TypicalEvents.java
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -20,41 +20,41 @@ import seedu.address.model.participant.Participant;
 public class TypicalEvents {
     public static final Participant SAMPLE_PARTICIPANT = new ParticipantBuilder().build();
 
-    public static final Event SAMPLE_EVENT = new Event(new EventName("Sleep"), new EventDate("2022-09-18"),
+    public static final Event SAMPLE_EVENT = new Event(new EventName("Sleep"), new EventDate("2021-09-18"),
             new EventTime("1000"));
-    public static final Event ANOTHER_EVENT = new Event(new EventName("Sleep2"), new EventDate("2022-09-18"),
+    public static final Event ANOTHER_EVENT = new Event(new EventName("Sleep2"), new EventDate("2021-09-18"),
             new EventTime("1001"));
     public static final Event SAMPLE_EVENT_2 = new Event(new EventName("Random Event 1"),
             new EventDate("1-1-1"), new EventTime("0001"));
-    public static final Event SAMPLE_EVENT_3 = new Event(new EventName("Sleep"), new EventDate("2022-09-18"),
+    public static final Event SAMPLE_EVENT_3 = new Event(new EventName("Sleep"), new EventDate("2021-09-18"),
             new EventTime("1002"), false, Collections.singletonList(new ParticipantBuilder().build()));
     public static final Event MARATHON_NO_TIME = new Event(new EventName("24km Marathon"),
-            new EventDate("2022-10-5"), new EventTime());
+            new EventDate("2021-10-5"), new EventTime());
     public static final Event MARATHON_HAS_TIME = new Event(new EventName("24Km Marathon with Time"),
-            new EventDate("2022-10-5"), new EventTime("0800"));
+            new EventDate("2021-10-5"), new EventTime("0800"));
     public static final Event CODE_FOR_SANITY = new Event(new EventName("Code For Sanity"),
-            new EventDate("2022-8-10"), new EventTime("0000"));
+            new EventDate("2021-8-10"), new EventTime("0000"));
     public static final Event SAMPLE_EVENT_SPECIFIED_TIME_AND_COMPLETION = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-18"), new EventTime("1002"), true,
+            new EventDate("2021-09-18"), new EventTime("1002"), true,
             Collections.singletonList(SAMPLE_PARTICIPANT));
     public static final Event SAMPLE_EVENT_SPECIFIED_TIME_AND_COMPLETION_COPY = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-18"), new EventTime("1002"), true,
+            new EventDate("2021-09-18"), new EventTime("1002"), true,
             Collections.singletonList(SAMPLE_PARTICIPANT));
     public static final Event SAMPLE_EVENT_DEFAULT_TIME_AND_COMPLETION = new Event(new EventName("Sleep again"),
-            new EventDate("2022-09-18"));
+            new EventDate("2021-09-18"));
     public static final Event SAMPLE_EVENT_COPY_DIFFERENT_NAME = new Event(new EventName("Sleeps"),
-            new EventDate("2022-09-18"), new EventTime("1000"));
+            new EventDate("2021-09-18"), new EventTime("1000"));
     public static final Event SAMPLE_EVENT_COPY_DIFFERENT_DATE = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-19"), new EventTime("1000"));
+            new EventDate("2021-09-19"), new EventTime("1000"));
     public static final Event SAMPLE_EVENT_COPY_DIFFERENT_TIME = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-18"), new EventTime("1001"));
+            new EventDate("2021-09-18"), new EventTime("1001"));
     public static final Event SAMPLE_EVENT_COPY_DIFFERENT_COMPLETION = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-18"), new EventTime("1000"), true, new ArrayList<>());
+            new EventDate("2021-09-18"), new EventTime("1000"), true, new ArrayList<>());
     public static final Event SAMPLE_EVENT_COPY_DIFFERENT_PARTICIPANTS = new Event(new EventName("Sleep"),
-            new EventDate("2022-09-18"), new EventTime("1000"), false,
+            new EventDate("2021-09-18"), new EventTime("1000"), false,
             Collections.singletonList(ALEX));
     public static final Event SAMPLE_EVENT_COPY_NO_TIME = new Event(new EventName("Slept"),
-            new EventDate("2022-09-18"));
+            new EventDate("2021-09-18"));
 
     private TypicalEvents() {}
 
